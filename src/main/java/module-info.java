7a8fb9838bfdcca38af870202cd50fd499877c79
AnthonyTyler27 <tylersvgs@gmail.com>
2024-10-10 09:25:39 -0400

    fixed module-info error

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.svgs {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.base;
+    requires java.sql;
     requires transitive javafx.graphics;
 
     opens com.svgs to javafx.fxml;
